a8315601d27f290b51c0e7daafb68c4161f63619
xucz <xgfd3@users.noreply.github.com>
2023-10-18 23:30:33 +0800

    [Android]Perfect code note.

--- Android/app/src/main/java/io/agora/app/karaoke/RoomListActivity.kt
+++ Android/app/src/main/java/io/agora/app/karaoke/RoomListActivity.kt
@@ -30,7 +30,7 @@ class RoomListActivity : AppCompatActivity() {
     private var viewBinding: RoomListActivityBinding? = null
     private val listAdapter by lazy { RoomListAdapter() }
     private var mList = listOf<AUIRoomInfo>()
-    private val mUserId = Random().nextInt(99999999).toString()
+
     private val launcher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
@@ -54,10 +54,11 @@ class RoomListActivity : AppCompatActivity() {
         val config = AUICommonConfig()
         config.context = application
         config.host = BuildConfig.SERVER_HOST
-        config.userId = mUserId
+        // Randomly generate local user information
+        config.userId = randomUserId()
         config.userName = randomUserName()
         config.userAvatar = randomAvatar()
-        // init AUIKit
+        // Setup karaokeUiKit
         KaraokeUiKit.setup(
             config = config, // must
             ktvApi = null,// option
@@ -65,6 +66,8 @@ class RoomListActivity : AppCompatActivity() {
             rtmClient = null // option
         )
     }
+
+
 
     private fun initView() {
         setTheme(ThemeId)
@@ -200,6 +203,8 @@ class RoomListActivity : AppCompatActivity() {
         val randomValue = Random().nextInt(8) + 1
         return "https://accktvpic.oss-cn-beijing.aliyuncs.com/pic/sample_avatar/sample_avatar_${randomValue}.png"
     }
+
+    private fun randomUserId() = Random().nextInt(99999999).toString()
 
     private fun randomRoomName(): String {
         val randomValue = (Random().nextInt(9) + 1) * 10000 + Random().nextInt(10000)
